6127c85630a90de37e834dcccbe6e5c7ca183dce
Damodar Khanal <deepakkhanal931@gmail.com>
2026-01-07 12:10:53 +0545

    Change the Favicon

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,7 +1,20 @@
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "Shakta Technology",
+  description: "Software Agency",
+  icons:{
+    icon: "/logo/shaktalogo.ico"
+    
+  }
+};
+
 export default function UserLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  
   return <>{children}</>;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 
 
 import type { Metadata } from "next";
-import {  Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import ClientLayout from "@/components/ClientLayout";
 import { ThemeProvider } from "@/components/ui/ThemeProvider";
@@ -10,15 +10,15 @@ import { ThemeProvider } from "@/components/ui/ThemeProvider";
 const poppins = Poppins({
   variable: "--font-poppins",
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600" , "700", "800"]
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"]
 });
 
 export const metadata: Metadata = {
   title: "Shakta Technology",
   description: "Software Agency",
-  icons:{
-    icon: "/logo/shaktalogo.svg"
-    
+  icons: {
+    icon: "/logo/shaktalogo.ico"
+
   }
 };
 
@@ -30,20 +30,35 @@ export default function RootLayout({
 
 
 
-  
+
   return (
     <html lang="en" suppressHydrationWarning>
+     <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              name: "Shakta Technology",
+              url: "https://shaktatechnology.com",
+              logo: "https://shaktatechnology.com/logo.png",
+            }),
+          }}
+        />
+      </head>
+
       <body className={` ${poppins.variable}  font-poppins antialiased`}>
-          <ThemeProvider
-                attribute="class"
-                defaultTheme="system"
-                enableSystem
-                disableTransitionOnChange
-              >
-              <ClientLayout>  
-                {children}
-              </ClientLayout>
-          </ThemeProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ClientLayout>
+            {children}
+          </ClientLayout>
+        </ThemeProvider>
       </body>
     </html>
   );
